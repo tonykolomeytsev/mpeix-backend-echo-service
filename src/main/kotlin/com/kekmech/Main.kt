66be9be60d8c02e6e47445eb007c9fc4bfc01b61
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-08-01 13:11:35 +0300

    Fix echo

--- src/main/kotlin/com/kekmech/Main.kt
+++ src/main/kotlin/com/kekmech/Main.kt
@@ -7,36 +7,35 @@ import io.ktor.features.Compression
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
-import io.ktor.routing.get
+import io.ktor.routing.*
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.util.toMap
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, port = GlobalConfig.port) {
+    val server = embeddedServer(Netty, port = GlobalConfig.port) {
         install(DefaultHeaders)
         install(Compression)
         install(CallLogging)
 
         routing {
+            get("/helloworld") {
+                call.respond(HttpStatusCode.OK, "hello world")
+            }
             get("/") {
                 call.respondText(ContentType.Text.Plain, HttpStatusCode.OK) {
                     call.request.headers
                         .toMap()
                         .toList()
-                        .joinToString("\n") { it.first + ": " + it.second }
-                }
-            }
-            post("/") {
-                call.respondText(ContentType.Text.Plain, HttpStatusCode.OK) {
-                    call.request.headers
-                        .toMap()
-                        .toList()
-                        .joinToString("\n") { it.first + ": " + it.second }
+                        .joinToString("\n") {
+                            it.first + ": " + it.second.joinToString(", ")
+                        }
                 }
             }
         }
     }
+    server.start(wait = true)
 }
